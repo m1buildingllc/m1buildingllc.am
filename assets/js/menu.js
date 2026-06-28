169f03f6b4c5bb5b26dcda1de02205258954e2d1
m1buildingllc <asaturasatryan98@gmail.com>
2026-06-28 13:00:52 +0400

    Update menu.js

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,1 +1,12 @@
+const menuToggle = document.getElementById("menuToggle");
+const nav = document.getElementById("mainMenu");
 
+menuToggle.addEventListener("click", () => {
+    nav.classList.toggle("active");
+});
+
+document.querySelectorAll("#mainMenu a").forEach(link => {
+    link.addEventListener("click", () => {
+        nav.classList.remove("active");
+    });
+});
