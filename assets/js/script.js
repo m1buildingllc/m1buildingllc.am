381ec5182bdde06d7ee471cb64873543b9e9dd28
m1buildingllc <asaturasatryan98@gmail.com>
2026-07-10 18:06:04 +0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -336,3 +336,17 @@ sendBtn.innerHTML = "Send Message";
 });
 
 });
+
+const slides=document.querySelectorAll(".hero-slide");
+
+let current=0;
+
+setInterval(()=>{
+
+    slides[current].classList.remove("active");
+
+    current=(current+1)%slides.length;
+
+    slides[current].classList.add("active");
+
+},15000);
